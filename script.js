3619a81548f209c7f57dbe70aeca75df33f25a07
Shiree998 <118954453+Shiree998@users.noreply.github.com>
2024-07-19 12:04:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+/getting elelment
+const nav=document.querySelector("nav");
+     toggleBtn=nav.querySelector(".toggle-btn");
+
+  toggleBtn.addEventListener("click", ()=>{
+    nav.classList.toggle("open");
+  });
+  
+  //js code to make draggable nav
+  function onDrag({movementY}){
+    const navStyle =window.getComputedStyle(nav);
+          navTop=parseInt(navStyle.top);
+          navHeight=parseInt(navStyle.height);
+          windHeight=window.innerHeight;
+    nav.style.top=navTop>0?`${navTop+movementY}px`:"1px";
+    if(navStyle>windHeight-navHeight){
+        nav.style.top= `${windHeight - navHeight}`;
+    }
+}
+
+  nav.addEventListener("mousedown", () =>{
+    nav.addEventListener("mousemove", onDrag)
+  });
+  nav.addEventListener("mouseleave", () =>{
+    nav.removeEventListener("mousemove", onDrag)
+  });
